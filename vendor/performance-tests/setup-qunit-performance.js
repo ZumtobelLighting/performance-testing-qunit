ce3e7d5f37c4e20f30b2cd3e4dff5029a7aa0c76
Calvin Flegal <cflegal@digitallumens.com>
2015-09-16 11:32:05 -0400

    invert performance flag

--- vendor/performance-tests/setup-qunit-performance.js
+++ vendor/performance-tests/setup-qunit-performance.js
@@ -1,9 +1,9 @@
 jQuery(window).load(function() {
   if (!window.QUnit) return;
-  QUnit.config.urlConfig.push({ id: 'includeperf', label: 'Performance'});
+  QUnit.config.urlConfig.push({ id: 'noperf', label: 'Skip performance'});
   var TestLoader = require('ember-cli/test-loader')['default'];
   TestLoader.prototype.shouldLoadModule = function(moduleName) {
     return moduleName.match(/\/.*[-_]test$/) || (!QUnit.urlParams.nojshint && moduleName.match(/\.jshint$/)) || 
-      (QUnit.urlParams.includeperf && moduleName.match(/\/.*[-_]perf$/))
+      (!QUnit.urlParams.noperf && moduleName.match(/\/.*[-_]perf$/))
   };
 });
